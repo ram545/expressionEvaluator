844f16cebcb6a08448422955fcbe2996851d2ea8
Rama Krishna Reddy Sabbella <20751687+ram545@users.noreply.github.com>
2019-01-28 15:28:29 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,74 +1,86 @@
+/**********************************************************************
+* FILENAME:        stack.cpp
+*
+* DESCRIPTION/PURPOSE:
+* Design and implementation of stack
+*
+* AUTHOR: 20020298 | Rama Krishna | sabbella.krishna@wipro.com
+* START DATE: 01/25/2019
+* SUBMISSION DATE: 01/25/2019
+**********************************************************************/
 #include<iostream>
+#include<cstring>
+#define cap 15
 using namespace std;
 
-template<typename T>
+template <typename T>
 class Stack{
-	private:
-		T *arr;
-		int top;
-		int capacity;
-	public:
-		Stack(int Size=15){
-			arr = new T[Size];
-			top = -1;
-			capacity = Size;
-		}
-		~Stack(){
-			delete[] arr;
-			top=0;
-			capacity=0;
-		}
-		void push(T var){
-			if(isFull())
-				cout << "Stack is full" << endl;
-			else
-				arr[++top]=var;
-		}
-		T pop(){
-			if(isEmpty()){
-				cout << "Stack is empty" << endl;
-				return NULL;
-			}
-			else
-				return arr[top--];
-		}
-		T peek(){
-			if(isEmpty()){
-				cout << "Stack is empty" << endl;
-				return NULL;
-			}
-			else
-				return arr[top];
-		}
-		bool isEmpty(){
-			return top==-1;
-		}
-		bool isFull(){
-			return top==capacity-1;
-		}
+    private:
+        int capacity;
+        T *arr;
+        int top;
+    public:
+        Stack(int len=cap){
+            arr = new T[len+1];
+            top = -1;
+            capacity = len;
+        }
+        ~Stack(){
+            delete[] arr;
+            top=0;
+            capacity=0;
+        }
+
+        void push(T c){
+            if(isFull())
+                cout << "Stack is Full" << endl;
+            else
+                arr[++top]=c;
+        }
+
+        T pop(){
+            if(isEmpty()){
+                cout << "Stack is empty" << endl;
+                return NULL;
+            }
+            else
+                return arr[top--];
+        }
+
+        T peek(){
+            if(isEmpty()){
+                cout << "Stack is empty" << endl;
+                return NULL;
+            }
+            else
+                return arr[top];
+        }
+
+        bool isEmpty(){
+            return top==-1;
+        }
+
+        bool isFull(){
+            return top==capacity-1;
+        }
 };
 
-template <typename U>
-class node{
-	private:
-		U root;
-		node *right;
-		node *left;
-	public:
-		node(U var){
-			root=var;
-			right=NULL;
-			left=NULL;
-		}
-		node(){
-			root=0;
-			right=NULL;
-			left=NULL:
-		}
-		void setRight(node a){
-			right=a;
-		}
-		void setLeft(node b){
-			left=b;
-		}
-}
+struct node{
+    string data;
+    node *right;
+    node *left;
+    void setValue(string val){
+        data=val;
+        right=NULL;
+        left=NULL;
+    }
+    bool isLeaf(node val){
+        if(val.right==NULL && val.left==NULL)
+            return 1;
+        return 0;
+    }
+};
+
+
+
+
